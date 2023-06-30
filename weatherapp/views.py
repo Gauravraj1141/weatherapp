28d99e01def4b77b21f86e9d5fd139c5f64c1ff9
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-06-30 18:32:57 +0530

    remove some known issues

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,38 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 import requests
 from datetime import datetime
 from .models import Weather
 
 def ShowWeather(request):
-        print(request.method)
         if request.method == "POST":
-            lat = request.POST.get('lat')
-            lon = request.POST.get('lon')
+            try:
+                lat = request.POST.get('lat')
+                lon = request.POST.get('lon')
 
-            url = "http://api.openweathermap.org/data/2.5/forecast"
-            params = {
-                "lat": lat,
-                "lon": lon,
-                "appid": "e78a372131b4e39a6dbce54cc156c407"
-            }
+                try:
+                     weather = Weather.objects.get(lat=lat, lon=lon)
+                     weather_data = {"temp":weather.temperature,"pressure":weather.pressure,"Humidity":weather.humidity,"wind":weather.wind,"weather":weather.weather,"main_weather":weather.main_weather,"icon":weather.icon,"time":weather.time, "city":weather.city}
+                except:
+                    url = "http://api.openweathermap.org/data/2.5/forecast"
+                    params = {
+                        "lat": lat,
+                        "lon": lon,
+                        "appid": "e78a372131b4e39a6dbce54cc156c407"
+                    }
 
-            response = requests.get(url, params=params)
-            weather_details = response.json()['list'][1]
+                    response = requests.get(url, params=params)
+                    weather_details = response.json()['list'][1]
 
-            datetime_object = datetime.strptime(str(weather_details['dt_txt']), '%Y-%m-%d %H:%M:%S')
+                    datetime_object = datetime.strptime(str(weather_details['dt_txt']), '%Y-%m-%d %H:%M:%S')
 
-            # Extract the time component from the datetime object
-            time = datetime_object.time()
+                    # Extract the time component from the datetime object
+                    time = datetime_object.time()
 
-            weather_data = {"temp":int((weather_details['main']['temp']) - 273.15),"pressure":weather_details['main']['pressure'],"Humidity":weather_details['main']['humidity'],"wind":round((weather_details['wind']['speed']*3.6),2),"weather":weather_details['weather'][0]['description'],"main_weather":weather_details['weather'][0]['main'],"icon":weather_details['weather'][0]['icon'],"time":time, "city":response.json()['city']['name']}
+                    weather_data = {"temp":int((weather_details['main']['temp']) - 273.15),"pressure":weather_details['main']['pressure'],"Humidity":weather_details['main']['humidity'],"wind":round((weather_details['wind']['speed']*3.6),2),"weather":weather_details['weather'][0]['description'],"main_weather":weather_details['weather'][0]['main'],"icon":weather_details['weather'][0]['icon'],"time":time, "city":response.json()['city']['name']}
 
-            # Adding data on database 
+                    # Adding data on database if this data is not present in database
 
-            data = Weather.objects.create(temperature = weather_data['temp'], pressure = weather_data['pressure'], humidity = weather_data['Humidity'], wind = weather_data['wind'], main_weather = weather_data['main_weather'], weather = weather_data['weather'], icon = weather_data['icon'] , time = weather_data['time'] , city = weather_data['city'], lat = lat, lon = lon)
+                    data = Weather.objects.create(temperature = weather_data['temp'], pressure = weather_data['pressure'], humidity = weather_data['Humidity'], wind = weather_data['wind'], main_weather = weather_data['main_weather'], weather = weather_data['weather'], icon = weather_data['icon'] , time = weather_data['time'] , city = weather_data['city'], lat = lat, lon = lon)
 
+                
+                context = {"weather_data":weather_data}
+                return render(request,'index.html',context)
             
-            context = {"weather_data":weather_data}
-            return render(request,'index.html',context)
+            except Exception as e:
+                 return HttpResponse(e)
         
         return render(request,'index.html')
 
