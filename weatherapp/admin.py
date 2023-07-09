3a32ccda4b28312124b43dc29ac637707c34c800
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2023-07-09 11:17:45 +0530

    changes required

--- weatherapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Weather
-
-@admin.register(Weather)
-class WeatherAdmin(admin.ModelAdmin):
-    fields = [ 'temperature', 'pressure', 'humidity', 'wind', 'main_weather', 'weather', 'icon', 'time', 'city','lat','lon','created_at']
-    
-
